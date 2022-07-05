b57eb7bb4445c9b6c8268b01ea31903954840d56
arvariable <35440657+AashishVarier@users.noreply.github.com>
2022-07-05 08:23:08 +0530

    Update 21-Merge Two Sorted Lists.java

--- 21-Merge Two Sorted Lists.java
+++ 21-Merge Two Sorted Lists.java
@@ -12,10 +12,10 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) { //l1 and l2 represent the reference address of the head
         
-        ListNode dummyList = new ListNode();
-        ListNode cur = dummyList; //avoid edge case of inserting into empty list
+        ListNode dummyNode = new ListNode();
+        ListNode cur = dummyNode; //avoid edge case of inserting into empty list
           
-        while(l1 != null & l2 != null){ //compare both LinkedList node till either of their  node reference is null && append the smaller node reference  to dummyList as its next node
+        while(l1 != null & l2 != null){ //compare both LinkedList node till either of their  node reference is null && append the smaller node reference  to dummyNode as its next node
             if(l1.val < l2.val){
                cur.next = l1;
                l1 = l1.next;
@@ -27,7 +27,7 @@ class Solution {
             
             cur = cur.next;
         }
-      //total number of node in L2 might be more than L1 ,append the  L2 reference after while() as dummyList next or vice versa. 
+      //total number of node in L2 might be more than L1 ,append the  L2 reference after while() as dummyNode next or vice versa. 
 	  //OR L1 / L2 could be both null or either one null - so wont enter while() loop
 	 
        if(l1 == null){
@@ -37,7 +37,7 @@ class Solution {
             cur.next =  l1;
         }
 
-        return dummyList.next;
+        return dummyNode.next;
 
     }
     
