df70bc0b701975d1f4984a17cc12033cec48a3b2
arvariable <35440657+AashishVarier@users.noreply.github.com>
2022-07-07 07:32:29 +0530

    Create 543-Diameter of Binary Tree.java

---
+++ 543-Diameter of Binary Tree.java
@@ -0,0 +1,40 @@
+//543. Diameter of Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    int maxDia = 0; 
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        dfs(root);
+        return maxDia ;
+        
+    }
+    
+    public int dfs(TreeNode root){
+        if(root == null ){
+            return 0;
+        } 
+        
+            int left = dfs(root.left) ;
+            int right = dfs(root.right);
+            maxDia = Math.max(maxDia, left + right);
+            return 1 + Math.max(left, right);
+       
+    }
+}
