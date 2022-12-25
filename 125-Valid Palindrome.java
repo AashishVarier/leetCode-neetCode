2c634dfa7e6cd9c08d73719bb0c3861ff3bc54cf
arvariable <35440657+AashishVarier@users.noreply.github.com>
2022-12-25 14:08:56 +0530

    Update 125-Valid Palindrome.java

--- 125-Valid Palindrome.java
+++ 125-Valid Palindrome.java
@@ -30,3 +30,38 @@ class Solution {
         
     }
 }
+
+
+//
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        String revString,toCheck;
+        
+        toCheck = isAlpha(s);
+        revString = new StringBuilder(toCheck).reverse().toString();
+
+        if(revString.equals(toCheck)){
+        return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String isAlpha(String str){
+        char c;
+        StringBuilder sb = new StringBuilder();
+
+        for(int i =0; i< str.length(); i++)
+        {   
+            c = str.charAt(i);
+            if( Character.isLetterOrDigit(c)){
+                sb.append(c);
+            }
+            
+        }
+       return sb.toString().toLowerCase();
+
+    }
+}
