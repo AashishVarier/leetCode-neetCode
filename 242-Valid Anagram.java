77b40803f2c4a351052e0d1ad4772060d37c13d1
arvariable <35440657+AashishVarier@users.noreply.github.com>
2023-01-05 08:20:16 +0530

    Update 242-Valid Anagram.java

--- 242-Valid Anagram.java
+++ 242-Valid Anagram.java
@@ -24,3 +24,28 @@ class Solution {
         return true;
     }
 }
+
+
+//Using Hashmap for Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> smap=new HashMap<>();
+
+        if(s.length() != t.length() ){
+            return false;}
+
+        for(int i=0;i<t.length();i++){
+            smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
+            smap.put(t.charAt(i),smap.getOrDefault(t.charAt(i),0)-1);
+
+        }
+
+        for(char c:smap.keySet()){
+            if(smap.get(c)!=0){
+                return false;}
+        }
+        
+        return true;
+    }
+}
