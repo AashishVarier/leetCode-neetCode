4acb7b3fefd3059c9d7dbc0ccbe86b525092a38e
arvariable <35440657+AashishVarier@users.noreply.github.com>
2023-10-03 00:35:18 +0530

    Update 217. Contains Duplicate.py

--- Python3/217. Contains Duplicate.py
+++ Python3/217. Contains Duplicate.py
@@ -1,3 +1,14 @@
+##T: O(n^2)  S: O(1)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if(nums[i] == nums[j]):
+                    return True
+        return False
+        
+
+##T: O(n) S: O(n)
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         checkSet = set()
