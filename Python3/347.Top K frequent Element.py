b7d6e6f49973051015d55c0082320f36f1d602d3
arvariable <35440657+AashishVarier@users.noreply.github.com>
2024-05-08 23:34:49 +0530

    Create 347.Top K frequent Element.py

---
+++ Python3/347.Top K frequent Element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        countDict = {}
+        res =[]
+
+        for i in nums:
+            countDict[i] = 1 + countDict.get(i, 0)
+            
+        pyListComprehension = [j for j in sorted(countDict, key = countDict.get, reverse = True)] #pyListComprehension returns type list , sorted(a, key with which to sort, boolean for reverse)
+        for l in range(k):
+            res.append(pyListComprehension[l])
+        
+        return res
+            
+            
+
+       
+
+        
