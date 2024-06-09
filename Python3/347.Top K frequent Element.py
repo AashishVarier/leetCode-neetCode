0e35571c7f0c8b539ffdacbe2235c89dc484f7fb
arvariable <35440657+AashishVarier@users.noreply.github.com>
2024-06-09 18:26:05 +0530

    Update 347.Top K frequent Element.py

--- Python3/347.Top K frequent Element.py
+++ Python3/347.Top K frequent Element.py
@@ -14,7 +14,47 @@ class Solution:
 
 #T: o(n  + nlogn)
             
-            
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        countD = {}
+        ans = []
+        freqList = [[] for i in range(len(nums) + 1)]
+
+        for i in range(len(nums)):
+            countD[nums[i]]  = 1 + countD.get(nums[i] , 0)
+        print(countD)
+        for key,val in countD.items():
+            freqList[val].append(key)
+        print(freqList)
+
+        for i in range(len(freqList) - 1 , 0 , -1):
+            for j in freqList[i]:
+                ans.append(j)
+                if len(ans) == k:
+                    return ans
+
+#T: O(n)
+
+#not mine but can use heap
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+        for num in nums:
+            freq[num] = 1 + freq.get(num, 0)
+        
+        heap = []
+        for num, freq in freq.items():
+            heapq.heappush(heap, (freq,num)) #keep the smallest value at heap[0], elements sorted based on the first element on the tuple.
+        
+        most_freq = heapq.nlargest(k, heap) #find nlargest numbers T:O(nlog(t)) 
+        
+        result = [num for freq, num in most_freq]
+        return result
+
+
+        
 
        
 
