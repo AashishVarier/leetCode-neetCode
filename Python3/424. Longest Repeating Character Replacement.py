dbda56e504a4216448f55b45e55ea469f0f2b427
arvariable <35440657+AashishVarier@users.noreply.github.com>
2024-12-04 21:53:46 +0530

    Update 424. Longest Repeating Character Replacement.py

--- Python3/424. Longest Repeating Character Replacement.py
+++ Python3/424. Longest Repeating Character Replacement.py
@@ -14,3 +14,41 @@ class Solution:
     
     #T: O(26*n) = O(n)
     #S: O(n)
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        for l in range(len(s)):
+            count, maxFreq = {}, 0
+            for r in range(l,len(s)):
+                count[s[r]] = 1 + count.get(s[r], 0)
+                maxFreq = max(maxFreq,count[s[r]] )
+                if (r - l + 1 - maxFreq ) <= k:
+                    res = max(res, r - l + 1 )
+        
+        return res
+        #T: O(n^2) n: length of string
+        #S: O(m) m: uniqe char in string
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0 
+        charSet = set(s)
+
+        for c in charSet:
+            count = l = 0
+            for r in range(len(s)):
+                if c == s[r]:
+                    count +=1
+                
+                while (((r - l + 1) - count) > k):
+                    if c == s[l]:
+                        count -= 1
+                    l +=1
+                
+                res = max(res, (r - l + 1))
+        
+        return res
+    
+    #T: O(mn) n: length of string, m: uniqe char in string
+        #S: O(m) 
