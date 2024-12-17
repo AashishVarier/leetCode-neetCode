6795d5549b8bf4840d8bbda24dcd23c0cd397030
arvariable <35440657+AashishVarier@users.noreply.github.com>
2024-12-17 21:23:03 +0530

    Update string encode and decode.py

--- Python3/string encode and decode.py
+++ Python3/string encode and decode.py
@@ -1,22 +1,24 @@
+#T:O(n)
+
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        newStr = ""
+        eStr = ""
         for i in strs:
-            newStr =newStr+ str(len(i)) + '#' + str(i)
-        return newStr
-
+            eStr += str(len(i)) + "#" + i
+        print (eStr)
+        return eStr
 
     def decode(self, s: str) -> List[str]:
-        res = []
+        dList = []
         i = 0
         while i < len(s):
             j = i
-            while s[j]!= '#':
-                j +=1
+            while s[j] != '#':
+                j +=1    
             length = int(s[i:j])
-            res.append(s[j+1: j+1 + length])
-            i=j+1 + length
-        return res
-
-#T:O(n)
+            i = j + 1
+            j = i + length
+            dList.append(s[i:j])
+            i=j
+        return dList
