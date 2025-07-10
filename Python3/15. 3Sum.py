adc85738aa1a5b072249bede6bb596e28e08f9d3
arvariable <35440657+AashishVarier@users.noreply.github.com>
2025-07-10 09:38:21 +0530

    Update 15. 3Sum.py

--- Python3/15. 3Sum.py
+++ Python3/15. 3Sum.py
@@ -1,3 +1,4 @@
+#2Sum 2 problem in loop2
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans = []
@@ -11,7 +12,6 @@ class Solution:
             while l<r: #loop 2
                 threeSum = v + nums[l] + nums[r]
                 
-
                 if threeSum > 0 :
                     r-=1
                 elif threeSum < 0:
@@ -28,3 +28,17 @@ class Solution:
     #T: O(n^2) + O(nlogn) + O(n)= ((loop1*loop2)+ sort + loop3)=  O(n^2) 
     #(why loop3 is O(n) = https://stackoverflow.com/questions/60009657/big-o-complexity-of-three-nested-loops-with-the-last-loop-in-an-if-statement)
     #S: O(n)
+
+#brute force
+    class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        tmp = [nums[i], nums[j], nums[k]]
+                        res.add(tuple(tmp))
+        return [list(i) for i in res]
+        #T : O(n^3)
